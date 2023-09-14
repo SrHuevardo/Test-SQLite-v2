894a93dd6d52e839cfbe58037acf2ec6b8020eee
SrHuevardo <nicolasfsacco@gmail.com>
2023-09-14 19:27:31 -0300

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,211 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import Constants from "expo-constants";
+import * as SQLite from "expo-sqlite";
 
-export default function App() {
+// Esta función se utiliza para abrir una base de datos SQLite.
+function openDatabase() {
+  if (Platform.OS === "web") {
+    return {
+      transaction: () => {
+        return {
+          executeSql: () => {},
+        };
+      },
+    };
+  }
+
+  const db = SQLite.openDatabase("db.db"); // Abre o crea una base de datos llamada "db.db"
+  return db;
+}
+
+const db = openDatabase(); // Abre la base de datos SQLite
+
+// Este componente muestra una lista de elementos completados o por hacer.
+function Items({ done: doneHeading, onPressItem }) {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    // Realiza una transacción en la base de datos SQLite para seleccionar elementos completados o por hacer.
+    db.transaction((tx) => {
+      tx.executeSql(
+        `select * from items where done = ?;`, // Consulta SQL para seleccionar elementos según su estado (completados o por hacer)
+        [doneHeading ? 1 : 0], // Parámetro para la consulta SQL, 1 para completados y 0 para por hacer
+        (_, { rows: { _array } }) => setItems(_array) // Actualiza el estado "items" con el resultado de la consulta
+      );
+    });
+  }, []);
+
+  const heading = doneHeading ? "Completed" : "Todo";
+
+  if (items === null || items.length === 0) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.sectionContainer}>
+      <Text style={styles.sectionHeading}>{heading}</Text>
+      {items.map(({ id, done, value }) => (
+        <TouchableOpacity
+          key={id}
+          onPress={() => onPressItem && onPressItem(id)}
+          style={{
+            backgroundColor: done ? "#1c9963" : "#fff",
+            borderColor: "#000",
+            borderWidth: 1,
+            padding: 8,
+          }}
+        >
+          <Text style={{ color: done ? "#fff" : "#000" }}>{value}</Text>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 }
 
+export default function App() {
+  const [text, setText] = useState(null);
+  const [forceUpdate, forceUpdateId] = useForceUpdate();
+
+  useEffect(() => {
+    // Crea la tabla "items" en la base de datos si no existe.
+    db.transaction((tx) => {
+      tx.executeSql(
+        "create table if not exists items (id integer primary key not null, done int, value text);"
+      );
+    });
+  }, []);
+
+  const add = (text) => {
+    // ¿Está el campo de texto vacío?
+    if (text === null || text === "") {
+      return false;
+    }
+
+    // Realiza una transacción en la base de datos para insertar un nuevo elemento.
+    db.transaction(
+      (tx) => {
+        tx.executeSql("insert into items (done, value) values (0, ?)", [text]); // Inserta un nuevo elemento con estado "por hacer"
+        tx.executeSql("select * from items", [], (_, { rows }) =>
+          console.log(JSON.stringify(rows))
+        );
+      },
+      null,
+      forceUpdate
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>SQLite Example</Text>
+
+      {Platform.OS === "web" ? (
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <Text style={styles.heading}>
+            Expo SQLite is not supported on web!
+          </Text>
+        </View>
+      ) : (
+        <>
+          <View style={styles.flexRow}>
+            <TextInput
+              onChangeText={(text) => setText(text)}
+              onSubmitEditing={() => {
+                add(text);
+                setText(null);
+              }}
+              placeholder="what do you need to do?"
+              style={styles.input}
+              value={text}
+            />
+          </View>
+          <ScrollView style={styles.listArea}>
+            <Items
+              key={`forceupdate-todo-${forceUpdateId}`}
+              done={false}
+              onPressItem={(id) =>
+                db.transaction(
+                  (tx) => {
+                    tx.executeSql(`update items set done = 1 where id = ?;`, [
+                      id,
+                    ]);
+                  },
+                  null,
+                  forceUpdate
+                )
+              }
+            />
+            <Items
+              done
+              key={`forceupdate-done-${forceUpdateId}`}
+              onPressItem={(id) =>
+                db.transaction(
+                  (tx) => {
+                    tx.executeSql(`delete from items where id = ?;`, [id]);
+                  },
+                  null,
+                  forceUpdate
+                )
+              }
+            />
+          </ScrollView>
+        </>
+      )}
+    </View>
+  );
+}
+
+// Esta función personalizada devuelve una función de actualización forzada y su estado.
+function useForceUpdate() {
+  const [value, setValue] = useState(0);
+  return [() => setValue(value + 1), value];
+}
+
+// Estilos para la aplicación.
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: "#fff",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+  },
+  heading: {
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+  },
+  input: {
+    borderColor: "#4630eb",
+    borderRadius: 4,
+    borderWidth: 1,
+    flex: 1,
+    height: 48,
+    margin: 16,
+    padding: 8,
+  },
+  listArea: {
+    backgroundColor: "#f0f0f0",
+    flex: 1,
+    paddingTop: 16,
+  },
+  sectionContainer: {
+    marginBottom: 16,
+    marginHorizontal: 16,
+  },
+  sectionHeading: {
+    fontSize: 18,
+    marginBottom: 8,
   },
 });
